e7f0dfbbfff44c786bc72e16e84220be7c86c1fc
Muhammad Arshad <59274118+Muhammad-Arshad506@users.noreply.github.com>
2023-11-23 15:14:52 +0500

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,1 +1,25 @@
 print("jenkins pipelines is runing")
+
+
+"""
+pipeline {
+    agent {label 'my-node'}
+    
+    stages{
+        stage('Bulid') {
+            steps {
+                 git url:'https://github.com/Muhammad-Arshad506/jenkin.git', branch:'main'
+            }
+        }
+        stage('Run JOb') {
+            steps {
+            sh "python /home/muhammadarshad/workspace/test/python_test.py"
+        }
+            
+        }
+        
+    }
+}
+
+
+"""
